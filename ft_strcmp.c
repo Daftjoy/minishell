1b9336550da111f3f9ea16ebdd0b59ea6a5a4fed
Antonio Martin espinosa <antmarti@c1r9s4.42madrid.com>
2020-11-30 20:21:45 +0100

    forks starting to work

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: antmarti <antmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 23:15:25 by antmarti          #+#    #+#             */
-/*   Updated: 2020/10/29 12:19:31 by antmarti         ###   ########.fr       */
+/*   Updated: 2020/11/30 16:46:06 by antmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	unsigned char	*str1;
 	unsigned char	*str2;
 	unsigned int	i;
-	
+
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
--- path.c
+++ path.c
@@ -5,39 +5,51 @@ int	main(int argc, char **argv, char **env)
 {
 	(void)argc;
 	(void)argv;
+	pid_t childPID;
 	//(void)env;
 	int i;
 	char **paths;
 	char *p;
-	int j;
+	int	j;
+	char *s;
+	char *st;
 
 	i = 0;
-	p = malloc(100);
+	j = 1;
+	st = "hola";
 	while (env[i])
 	{
-		if (env[i][0] == 'P' && env[i][1] == 'A')
+		if (!ft_strcmp("PATH", ft_substr(env[i], 0, 4)))
 			paths = ft_split(env[i], ':');
 		i++;
 	}
-	i = 0;
-	while (paths[i])
+
+	while (get_next_line(0, &s))
 	{
-		/*
-		Ya nos guarda cada posibe path ahora hay que copiarla añadiéndole el nombre del comando
-		y probar a ejecutar con execve
-		 */
-		j = 0;
-		while (paths[i][j])
+		childPID = fork();
+		if (childPID >= 0)
 		{
-			p[j] = paths[i][j];
-			j++;
+			if (childPID != 0)
+			{
+				wait(NULL);
+			}
+			else
+			{
+				i = 0;
+				while (paths[i])
+				{
+					//get_next_line(0, &s);
+					p = malloc(100);
+					p = ft_strjoin(paths[i], "/");
+					p = ft_strjoin(p, (const char*)s);
+					execve(p, argv, env);
+					i++;
+					p = 0;
+					free(p);
+				}
+			}
 		}
-		p [j] = '/';
-		p [j + 1] = 'l';
-		p [j + 2] = 's';
-		p [j + 3] = '\0';
-		execve(p, argv, env);
-		i++;
+		free(s);
 	}
 	return (0);
 }
